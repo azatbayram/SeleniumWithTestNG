4a65fd7451622bcf305fcde4ff7aca8ece2885ae
azatbayram <bayramov.azat.tkm@gmail.com>
2023-11-08 18:22:25 +0300

    TestNG annotation

---
+++ src/test/java/tests/day3_testNG/firstTestNGTest.java
@@ -0,0 +1,16 @@
+package tests.day3_testNG;
+
+import org.testng.annotations.Test;
+
+public class firstTestNGTest {
+
+    @Test
+    public void test1(){
+        System.out.println("My first test case");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("My second test case");
+    }
+}
