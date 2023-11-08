69bba3f30c18a27034ed87e810aa5133b751a7a0
azatbayram <bayramov.azat.tkm@gmail.com>
2023-11-08 17:15:43 +0300

    Class Name Locator

---
+++ src/test/java/tests/day2_WebElementBasics_BasicLocators/classNameLocator.java
@@ -0,0 +1,23 @@
+package tests.day2_WebElementBasics_BasicLocators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utilities.WebDriverFactory;
+
+public class classNameLocator {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com/sign_up");
+
+        WebElement homeLink = driver.findElement(By.className("nav-link"));
+        homeLink.click();
+
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
+}
