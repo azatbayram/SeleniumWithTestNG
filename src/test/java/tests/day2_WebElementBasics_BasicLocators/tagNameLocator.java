da24be9403cf35b10e2393d52c62b0a1c0f87340
azatbayram <bayramov.azat.tkm@gmail.com>
2023-11-08 17:49:11 +0300

    Tag Name Locator

---
+++ src/test/java/tests/day2_WebElementBasics_BasicLocators/tagNameLocator.java
@@ -0,0 +1,29 @@
+package tests.day2_WebElementBasics_BasicLocators;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utilities.WebDriverFactory;
+
+public class tagNameLocator {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://practice.cydeo.com/sign_up");
+
+        WebElement fullNAmeInbox = driver.findElement(By.tagName("input"));
+        WebElement emailInbox = driver.findElement(By.name("email"));
+        WebElement signUpButton = driver.findElement(By.tagName("button"));
+
+        fullNAmeInbox.sendKeys("Azat Bayram");
+        emailInbox.sendKeys("example@gmail.com");
+        System.out.println(fullNAmeInbox.getAttribute("value"));
+        System.out.println(emailInbox.getAttribute("value"));
+        signUpButton.click();
+
+        Thread.sleep(3000);
+        driver.quit();
+    }
+}
