4baa9865f7defab5e9cc6c49fd639773983fb3a9
azatbayram <bayramov.azat.tkm@gmail.com>
2023-11-08 16:22:01 +0300

    verify email

---
+++ src/test/java/tests/day2_WebElementBasics/VerifyValueOfInput.java
@@ -0,0 +1,36 @@
+package tests.day2_WebElementBasics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utilities.WebDriverFactory;
+
+public class VerifyValueOfInput {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.get("https://practice.cydeo.com/forgot_password");
+
+        WebElement emailInputbox = driver.findElement(By.name("email"));
+
+        String expectedEmail = "example@gmail.com";
+        emailInputbox.sendKeys(expectedEmail);
+
+        String actualEmail = emailInputbox.getAttribute("value");
+
+        if(expectedEmail.equals(actualEmail)){
+            System.out.println("PASS");
+            System.out.println("expectedEmail = " + expectedEmail);
+            System.out.println("actualEmail = " + actualEmail);
+        }
+        else {
+            System.out.println("FAIL");
+            System.out.println("expectedEmail = " + expectedEmail);
+            System.out.println("actualEmail = " + actualEmail);
+        }
+
+        driver.quit();
+
+    }
+}
