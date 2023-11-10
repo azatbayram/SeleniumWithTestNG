f34ebe35ae972f69b02fbf02734deaa1660b485a
azatbayram <bayramov.azat.tkm@gmail.com>
2023-11-10 16:41:14 +0300

    dropdown element with Select class

---
+++ src/test/java/tests/day4_typeOfElements/selectDropDown.java
@@ -0,0 +1,48 @@
+package tests.day4_typeOfElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
+import java.util.List;
+
+public class selectDropDown {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+    @Test
+    public void test1(){
+        driver.get("https://practice.cydeo.com/dropdown");
+
+        WebElement state = driver.findElement(By.id("state"));
+
+        Select selectDropdown = new Select(state);
+
+        List<WebElement> options = selectDropdown.getOptions();
+
+        System.out.println("options.size() = " + options.size());
+
+        for (WebElement option : options) {
+            System.out.println(option.getText());
+        }
+
+        selectDropdown.selectByValue("AK");
+    }
+}
