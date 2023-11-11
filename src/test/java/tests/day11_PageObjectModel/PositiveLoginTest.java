d5a3a98b58ec2cf4c442cffef799650147e19f95
azatbayram <bayramov.azat.tkm@gmail.com>
2023-11-11 18:57:58 +0300

    Login as store manager in three way by using Page Object Model

--- src/test/java/tests/day11_PageObjectModel/PositiveLoginTest.java
+++ src/test/java/tests/day11_PageObjectModel/PositiveLoginTest.java
@@ -23,4 +23,45 @@ public class PositiveLoginTest extends TestBase {
         Assert.assertEquals(driver.getCurrentUrl(), "https://qa3.vytrack.com/");
     }
 
+    @Test
+    public void loginAsStoreManager(){
+
+        LoginPage loginPage = new LoginPage();
+
+        String username = ConfigurationReader.get("storemanager_username");
+        String password = ConfigurationReader.get("storemanager_password");
+
+        loginPage.usernameInput.sendKeys(username);
+        loginPage.passwordInput.sendKeys(password);
+        loginPage.loginButton.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa3.vytrack.com/");
+    }
+
+    @Test
+    public void loginAsStoreManager2(){
+
+        LoginPage loginPage = new LoginPage();
+
+        String username = ConfigurationReader.get("storemanager_username");
+        String password = ConfigurationReader.get("storemanager_password");
+
+        loginPage.login(username, password);
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa3.vytrack.com/");
+    }
+
+    @Test
+    public void loginAsStoreManager3(){
+
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.loginAsStoreManager();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa3.vytrack.com/");
+
+    }
+
+
+
 }
