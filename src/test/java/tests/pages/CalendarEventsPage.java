a8060815e732d8cd8cdbc8bcdb3d149b09bb842f
azatbayram <bayramov.azat.tkm@gmail.com>
2023-11-11 18:27:37 +0300

    Page Object Model

---
+++ src/test/java/tests/pages/CalendarEventsPage.java
@@ -0,0 +1,17 @@
+package tests.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class CalendarEventsPage extends BasePage {
+
+    public CalendarEventsPage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+    @FindBy(css = "[title='Create Calendar event']")
+    public WebElement createCalendarEvent;
+
+}
---
+++ src/test/java/tests/pages/ContactInfoPage.java
@@ -0,0 +1,20 @@
+package tests.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ContactInfoPage extends BasePage {
+
+    @FindBy(css = "div.pull-left>h1.user-name")
+    public WebElement fullName;
+
+    @FindBy(css = "a.phone")
+    public WebElement phone;
+
+    @FindBy(css = "a.email")
+    public WebElement eamil;
+
+
+
+
+}
