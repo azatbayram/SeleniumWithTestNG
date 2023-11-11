b8040f5c10f19267b5b6a475f256b3407bb90128
azatbayram <bayramov.azat.tkm@gmail.com>
2023-11-11 19:24:18 +0300

    Repeat options test

---
+++ src/test/java/tests/day11_PageObjectModel/RepeatOptionsTest.java
@@ -0,0 +1,113 @@
+package tests.day11_PageObjectModel;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tests.TestBase;
+import tests.pages.CalendarEventsPage;
+import tests.pages.CreateCalendarEventsPage;
+import tests.pages.DashboardPage;
+import tests.pages.LoginPage;
+import utilities.BrowserUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RepeatOptionsTest extends TestBase {
+
+     /**
+      *VERIFY RADIO BUTTONS
+     *Open Chrome browser
+     *Login as driver
+     *Go to Activities->Calendar Events
+     *Click on create calendar events
+     *Click on repeat
+     *Verify that repeat every days is checked
+     *verify that repeat weekday is not checked
+     * */
+
+     @Test
+    public void test(){
+
+         LoginPage loginPage = new LoginPage();
+
+         //Login as driver
+         loginPage.loginAsDriver();
+
+         DashboardPage dashboardPage = new DashboardPage();
+         //Go to Activities->Calendar Events
+         dashboardPage.navigateToModule("Activities","Calendar Events");
+
+         CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+         //Click on create calendar events
+         //wait for loader mask
+         calendarEventsPage.waitUntilLoaderScreenDisappear();
+         BrowserUtils.waitForClickablility(calendarEventsPage.createCalendarEvent,20);
+         calendarEventsPage.createCalendarEvent.click();
+
+         CreateCalendarEventsPage createCalendarEventsPage = new CreateCalendarEventsPage();
+         // Click on repeat
+         createCalendarEventsPage.repeat.click();
+
+         //Verify that repeat every days is checked
+         Assert.assertTrue(createCalendarEventsPage.days.isSelected(),"Verify repeat days is selected");
+         //verify that repeat weekday is not checked
+         Assert.assertFalse(createCalendarEventsPage.weekday.isSelected(),"Verify weekday is not selected");
+
+
+     }
+
+     /**
+      *VERIFY REPEAT OPTIONS
+      *Open Chrome browser
+      *Login as driver
+      *Go to Activities->Calendar Events
+      *Click on create calendar events button
+      *Click on repeat checkbox
+      *Verify that repeat options are Daily, Weekly, Monthly,Yearly(in this order)
+     **/
+
+    @Test
+    public void test2(){
+
+        LoginPage loginPage = new LoginPage();
+
+        //Login as driver
+        loginPage.loginAsDriver();
+
+        DashboardPage dashboardPage = new DashboardPage();
+        //Go to Activities->Calendar Events
+        dashboardPage.navigateToModule("Activities","Calendar Events");
+
+        CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+        //Click on create calendar events
+        //wait for loader mask
+        calendarEventsPage.waitUntilLoaderScreenDisappear();
+        BrowserUtils.waitForClickablility(calendarEventsPage.createCalendarEvent,25);
+        calendarEventsPage.createCalendarEvent.click();
+
+        CreateCalendarEventsPage createCalendarEventsPage = new CreateCalendarEventsPage();
+        // Click on repeat
+        createCalendarEventsPage.repeat.click();
+
+        Select repeatsDropdown = createCalendarEventsPage.repeatOptionsList();
+
+        // Verify that repeat options are Daily, Weekly, Monthly,Yearly(in this order)
+        List<String> expectedList = Arrays.asList("Daily","Weekly","Monthly","Yearly");
+
+        List<String> actualList = new ArrayList<>();
+
+        List<WebElement> actualOptions = repeatsDropdown.getOptions();
+        for (WebElement option : actualOptions) {
+            //get text of each element and add to actual List
+            actualList.add(option.getText());
+        }
+        //ready method for getting text of list of web elements
+        List<String> actualList2 = BrowserUtils.getElementsText(actualOptions);
+        //Verify that repeat options are Daily, Weekly, Monthly,Yearly(in this order)
+        Assert.assertEquals(actualList,expectedList,"Verify Daily weekly monthly yearly");
+
+    }
+}
